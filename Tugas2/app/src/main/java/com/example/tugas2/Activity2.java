8001f9500c91ee9061a0655606aa7e97a49f4a90
rahulsubagio <rahulsubagio99@gmail.com>
2021-04-04 21:04:42 +0700

    tambah tugas2 convert nilai

---
+++ Tugas2/app/src/main/java/com/example/tugas2/Activity2.java
@@ -0,0 +1,26 @@
+package com.example.tugas2;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Activity2 extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2);
+
+        TextView tvNama = (TextView) findViewById(R.id.tv_nama);
+        TextView tvNim  = (TextView) findViewById(R.id.tv_nim);
+        TextView tvNilai= (TextView) findViewById(R.id.tv_nilai);
+        Bundle bundle   = getIntent().getExtras();
+        String sNama    = bundle.getString("nama");
+        String sNim     = bundle.getString("nim");
+        String sNilai   = bundle.getString("nilai");
+        tvNama.setText(sNama);
+        tvNim.setText(sNim);
+        tvNilai.setText(sNilai);
+    }
+}
---
+++ Tugas2/app/src/main/java/com/example/tugas2/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.tugas2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final EditText etNama   = (EditText) findViewById(R.id.et_nama);
+        final EditText etNim    = (EditText) findViewById(R.id.et_nim);
+        final EditText etNilai  = (EditText) findViewById(R.id.et_nilai);
+        Button btnSubmit = (Button) findViewById(R.id.btn_submit);
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, Activity2.class);
+                i.putExtra("nama", etNama.getText().toString());
+                i.putExtra("nim", etNim.getText().toString());
+                String sNilai   = etNilai.getText().toString();
+                String hasil = "";
+                double nilai    = Double.parseDouble(sNilai);
+                if (nilai > 3.66 && nilai <= 4.00){
+                    hasil = "A";
+                } else if (nilai > 3.33 && nilai <= 3.66) {
+                    hasil = "A-";
+                } else if (nilai > 3.00 && nilai <= 3.33) {
+                    hasil = "B+";
+                } else if (nilai > 2.66 && nilai <= 3.00) {
+                    hasil = "B";
+                } else if (nilai > 2.33 && nilai <= 2.66) {
+                    hasil = "B-";
+                } else if (nilai > 2.00 && nilai <= 2.33) {
+                    hasil = "C+";
+                } else if (nilai > 1.66 && nilai <= 2.00) {
+                    hasil = "C";
+                } else if (nilai > 1.33 && nilai <= 1.66) {
+                    hasil = "C-";
+                } else if (nilai > 1.00 && nilai <= 1.33) {
+                    hasil = "D+";
+                } else if (nilai == 1.00) {
+                    hasil = "D";
+                }
+                i.putExtra("nilai", hasil);
+                startActivity(i);
+            }
+        });
+    }
+}
